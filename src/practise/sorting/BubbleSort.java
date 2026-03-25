785da8db5dc4cd3934f5bffa52765a94c2729e02
ashish-hexahealth <ashish.sharma@hexahealth.com>
2026-03-25 14:48:28 +0530

    refactor BubbleSort implementation for improved readability and efficiency

--- src/practise/sorting/BubbleSort.java
+++ src/practise/sorting/BubbleSort.java
@@ -7,20 +7,17 @@ And move greatest element at last position and then second greatest ele at secon
 public class BubbleSort {
 
     public static int[] bubbleSorting(int[] arr) {
-        // passes
-        for (int i = 0; i < arr.length - 1; i++) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
             boolean swapped = false; // a boolean var to run out further sorting if their was no swap in inner loop.
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) { // checking if left sibling is greater than right, then swap these, i.e to
-                                           // move it at last.
-                    // --------swapping--------
+            for (int j = 0; j < n - i - 1; j++) {
+                // checking if left sibling is greater than right, then swap these
+                if (arr[j] > arr[j + 1]) { 
                     UtilityClass.swapping(arr, j, j + 1);
                     swapped = true;
                 }
             }
-            // If no two elements were
-            // swapped by inner loop, then break outer loop, its meant our array is sorted
-            // now.
+            // If no two elements were swapped by inner loop, then break outer loop, its meant our array is sorted
             if (swapped != true)
                 break;
         }
